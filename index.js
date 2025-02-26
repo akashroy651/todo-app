d70a51ce9b57ccd3f478dcd2da9045235f4cca48
Akash Roy <akashroy65123@gmail.com>
2025-02-26 23:12:25 +0600

    updata

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
+///////  find the element
+const container = document.querySelector(".container");
+const todoForm = document.querySelector(".todo-form");
+const todoInput = document.querySelector("#inputTodo");
+const todoAddButton = document.querySelector("#addTodoButton");
+const todoLists = document.getElementById("lists");  
+
+    // add todo
+const addTodo = (event)=>{
+    event.preventDefault();
+   const todoValue = todoInput.value;
+
+
+   //  unique id
+   const todoId = Date.now().toString()
+   console.log(todoId);
+}
+
+
+//  adding listeners
+todoForm.addEventListener("submit", addTodo)
+
+
+
+ 
 
 
